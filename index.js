5bedba815f91335f3d01478f9dcd1483926d76cb
manhleodz <doanducmanh100@gmail.com>
2024-02-27 23:36:28 +0700

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3040",
+    origin: ["http://localhost:3040", "https://my-social-network-umber.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
